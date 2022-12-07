ef85ade1c92243cd76e06dd3880659beabb0fca2
Bwaweru12345 <bryanwaweru1076@example.com>
2022-12-07 14:45:42 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+function writeCards(name) {
+
+    let messages = [];
+    for(let i = 0; i < name.length; i++){
+    let message = `Thank you, ${name[i]}, for the wonderful surprise gift!`;
+    
+     messages.push(message);
+ 
+
+   
+     
+}
+
+return messages;
+}
+let count;
+function countDown(count){
+    while(count >=0){
+        console.log(count);
+        count--;
+        
+    }
+   
+}
+
+countDown(count);
